974378adb16e45c01b9a8570b4b9f3b8d6dfff89
hazemhemaily <hazem.hemily@gmail.com>
2021-03-26 03:50:13 +0200

    package.json build configuration

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://605d3c689e891912877b89c8--h-react-chat.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
